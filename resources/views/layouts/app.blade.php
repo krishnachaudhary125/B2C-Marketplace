0dd10ce5baf7bf5d609dc3934813c2f347538436
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-03-31 18:33:11 +0545

    Learned about blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>B2C Marketplace</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    @include('partials.header')
+    <div class="main-container">
+
+    </div>
+    @include('partials.footer')
+</body>
+
+</html>
